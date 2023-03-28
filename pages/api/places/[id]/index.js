f4cb557ab57138a512859394aa3848bea4c03ec3
Anja Siewert <anja.siewert@live.de>
2023-03-28 17:08:09 +0200

    add read, create and update

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,17 +1,22 @@
-import { places } from '../../../../lib/db.js';
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  if (!id) {
-    return;
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+    if (!place) {
+      return response.status(404).json({ message: "Place not found" });
+    }
+    return response.status(200).json(place);
   }
+  if ((request.method = "PATCH")) {
+    await Place.findByIdAndUpdate(id, {
+      $set: request.body,
+    });
 
-  const place = places.find((place) => place.id === id);
-
-  if (!place) {
-    return response.status(404).json({ status: 'Not found' });
+    return response.status(200).json({ message: "Place updated" });
   }
-
-  response.status(200).json(place);
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,20 @@
-import { places } from '../../../lib/db';
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
+  if (request.method === "POST") {
+    try {
+      await Place.create(request.body);
+      return response.status(201).json({ message: "Place created" });
+    } catch (error) {
+      return response.status(400).json({ message: "error" });
+    }
+  } else {
+    response.status(405).json({ error: "Method not allowed" });
+  }
 }
